dbf2ccf553b96888a85d7867bc78f5c97822df4d
saleem-ux <saleem_diab86@yahoo.com>
2021-04-26 15:04:26 -0700

    i finished the lab02

---
+++ js/app.js
@@ -0,0 +1,130 @@
+'use strict';
+let user = prompt('Hello, Could you please enter your name')
+alert(user + ', Welcome to my website')
+//console.log(user);
+
+let user1 = prompt('Do you know me?');
+switch (user1.toLowerCase()) {
+    case 'yes':
+        //console.log('yes , you do');
+        alert('Yes , I do');
+        break;
+
+    case 'y':
+         //console.log('yes , you do');
+         alert('Yes , I do');
+         break;
+        
+     case 'no':
+         //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+
+    case 'n':
+        //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+          
+
+}
+let user2 = prompt('Do you think this site will be beneficial?');
+switch (user2.toLowerCase()) {
+    case 'yes':
+        //console.log('yes , you do');
+        alert('Yes , I do');
+        break;
+
+    case 'y':
+        // console.log('yes , you do');
+         alert('Yes , I do');
+         break;
+        
+     case 'no':
+         //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+
+    case 'n':
+        //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+          
+
+}
+let user3 = prompt('Are you interested to know about my experience?');
+switch (user3.toLowerCase()) {
+    case 'yes':
+        //console.log('yes , you do');
+        alert('Yes , I do');
+        break;
+
+    case 'y':
+         //console.log('yes , you do');
+         alert('Yes , I do');
+         break;
+        
+     case 'no':
+        // console.log('no , you do not');
+         alert('No , I do not');
+         break;
+
+    case 'n':
+        //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+          
+
+}
+let user4 = prompt('Are you interested to know about my academic education?');
+switch (user4.toLowerCase()) {
+    case 'yes':
+        //console.log('yes , you do');
+        alert('Yes , I do');
+        break;
+
+    case 'y':
+         //console.log('yes , you do');
+         alert('Yes , I do');
+         break;
+        
+     case 'no':
+         //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+
+    case 'n':
+        //console.log('no , you do not');
+         alert('No , I do not');
+         break;
+          
+
+}
+let user5 = prompt('Would you like to share this website with your friends?');
+switch (user5.toLowerCase()) {
+    case 'yes':
+        //console.log('yes , you do');
+        alert('Yes , I will');
+        document.write(user + ' , hope you like my website')
+        break;
+
+    case 'y':
+         //console.log('yes , you do');
+         alert('Yes , I will');
+         document.write(user + ' , hope you like my website')
+         break;
+        
+     case 'no':
+         //console.log('no , you do not');
+         alert('No , I will not');
+         document.write(user + ' , hope you like my website')
+         break;
+
+    case 'n':
+        //console.log('no , you do not');
+         alert('No , I will not');
+         document.write(user + ' , hope you like my website')
+         break;
+          
+}
+
+
